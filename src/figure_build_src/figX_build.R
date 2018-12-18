9e85004f33c39db4906883535a854167da6f07bc
Ryan Berger PhD <33732189+rberger997@users.noreply.github.com>
2018-12-18 15:07:25 -0500

    updated figure build script

--- src/figure_build_src/figX_build.R
+++ src/figure_build_src/figX_build.R
@@ -7,14 +7,16 @@ library(here)
 ## -------------------------------------------------------------------------
 # STM mutants figure
 
+# Create folder for figures
+dir.create(here('figures/'))
+
+
 # Load ggplot objects
 mut_hallmark <- readRDS(file = here('img/ggplot_objects/gg_mut_hallmarkGSEA.rds'))
-mut_volcano <- readRDS(file = here('img/ggplot_objects/gg_mut_volcano.rds'))
 mut_pca <- readRDS(file = here('img/ggplot_objects/gg_mut_pcaplot.rds'))
 mut_venn_2h <- readRDS(file = here('img/ggplot_objects/gg_mut_venn_2h.rds'))
 mut_venn_8h <- readRDS(file = here('img/ggplot_objects/gg_mut_venn_8h.rds'))
-mut_bar_2h <- readRDS(file = here('img/ggplot_objects/gg_mut_reactome_bar_2h.rds'))
-mut_bar_8h <- readRDS(file = here('img/ggplot_objects/gg_mut_reactome_bar_8h.rds'))
+
 
 png(filename = here('figures/stm_muts_fig.png'),
     width = 12, height = 8, units = 'in', res = 300)
@@ -25,11 +27,8 @@ layout <- rbind(c(1, 1, 2, 2),
 
 grid.arrange(mut_pca,
              mut_hallmark+ggtitle('')+ylab(''),
-             #mut_volcano+theme(legend.position="none"),
              mut_venn_2h,
              mut_venn_8h,
-             #mut_bar_2h,
-             #mut_bar_8h,
              layout_matrix = layout)
 
 dev.off() 
@@ -39,12 +38,9 @@ dev.off()
 
 # Load ggplot objects
 ser_hallmark <- readRDS(file = here('img/ggplot_objects/gg_ser_hallmarkGSEA.rds'))
-ser_volcano <- readRDS(file = here('img/ggplot_objects/gg_ser_volcano.rds'))
 ser_pca <- readRDS(file = here('img/ggplot_objects/gg_ser_pcaplot.rds'))
 ser_venn_2h <- readRDS(file = here('img/ggplot_objects/gg_ser_venn_2h.rds'))
 ser_venn_8h <- readRDS(file = here('img/ggplot_objects/gg_ser_venn_8h.rds'))
-ser_bar_2h <- readRDS(file = here('img/ggplot_objects/gg_ser_reactome_bar_2h.rds'))
-ser_bar_8h <- readRDS(file = here('img/ggplot_objects/gg_ser_reactome_bar_8h.rds'))
 
 
 png(filename = here('figures/serovars_fig.png'),
@@ -60,8 +56,6 @@ grid.arrange(ser_pca,
              #ser_volcano+theme(legend.position="none"),
              ser_venn_2h,
              ser_venn_8h,
-             #ser_bar_2h,
-             #ser_bar_8h,
              layout_matrix = layout)
 
 dev.off() 
